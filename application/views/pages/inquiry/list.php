23e65791027941c6d8dc9c82fd0d4d18bd8e7c2b
AndyZheng <tianhua.zheng@theoutnet.cn>
2015-01-28 16:59:15 +0800

    a

--- application/views/pages/inquiry/list.php
+++ application/views/pages/inquiry/list.php
@@ -29,10 +29,29 @@ $html = '<div class="list-item ">' . $this->lang->line('createinquiry') .
 	      
 	    </div>
    
-   		<div class='product-list' id='question-container' style='display:none'>
-   		<div>Inquiry</div>
-   		<div id='question-list'></div>
+   		<div class='product-list' id='greeting-container' style='display:none'>
+   			<div>Greeting</div>
+   			<div id='greeting-list'></div>
    		</div>
+   		
+   		<div class='product-list' id='question-container' style='display:none'>
+   			<div>Inquiry</div>
+   			<div id='question-list'></div>
+   		</div>
+   		
+   		<div class='product-list' id='template-container' style='display:none'>
+   			<div>Template</div>
+   			<textarea  cols='60' rows='20' id='template-content'></textarea>
+   		</div>
+   		
+   		<div class="control-group">
+          <!-- Button -->
+          <div class="controls">
+            <button id='submit' type='button' class="btn btn-success" onclick='javascript:generateTemplate();'><i class="icon-white icon-hand-right"></i> <?php echo $this->lang->line('submit'); ?></button>
+          </div>
+        </div>
+	</div>
+	
     </div>
 <script>
 function showContent(id){
@@ -44,23 +63,46 @@ function showContent(id){
 		dataType: "json"
 		}).done(function(data){
 			$('#question-list').html('');
+			$('#greeting-list').html('');
 			if(!data)
 				return false;
-			var questions = data['questions'];
-			questions = data.questions;
+			var questions =  data.questions;
+			var greetings =  data.greetings;
 			if(questions)
 			{
 				$.each(questions,function(index,value){
 					var question = value['question'];
 					var qid = value['id'];
-					$('#question-list').append('<div><label><input type="radio" name="question_id" id="question_' + 
+					$('#question-list').append('<div><label><input type="checkbox" value="' + question + '" class="question_id" name="question_id_' + qid + '" id="question_' + 
 							qid
 					   + '"  />' + question +  '</label></div>');
 					})
 					$('#question-container').fadeIn();
 			}
-		});
-			
+			if(greetings)
+			{
+				$.each(greetings,function(index,value){
+					var greeting = value['content'];
+					var gid = value['id'];
+					$('#greeting-list').append('<div><label><input type="radio" value="' + greeting + '" class="greeting_id" name="greeting_id" id="greeting_' + 
+							gid
+					   + '"  />' + greeting +  '</label></div>');
+					})
+					$('#greeting-container').fadeIn();
+			}
+		});	
+		
 	
+}
+
+function generateTemplate(){
+var greeting = $('input.greeting_id:checked').val();
+var questions = '';
+$('input.question_id:checked').each(function(){
+	questions +=  $(this).val() + '&#13';
+});
+var html = '' + greeting + '&#13' + questions ;
+$('#template-content').html(html);
+$('#template-container').fadeIn();
 }
 	       </script>
