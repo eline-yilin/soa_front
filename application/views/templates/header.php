da65aa401fd96eb8cfbae5783b6988aed572dd06
yjhcfzx <andyzheng84@outlook.com>
2015-04-01 21:14:16 +0800

    add link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -56,6 +56,9 @@
            		</li>
            <?php endif;?>
            <?php endforeach;?>
+           		<li class='nav-item' id='nav-item-back'>
+           			<a href="<?php echo $this->config->item( 'base_url') , '../../soa_dash/' ;?>">后台</a>
+           		</li>
            		<li role="presentation" class="dropdown operation pull-right" >
            		<?php if(isset($user) && $user) :?>
 				    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
